3a6608251dca05aa4ee72ef272016654688d84e7
Imated <lineborder122@gmail.com>
2023-07-30 22:59:07 -0400

    Added Extra Sprite Variations

--- Ico Jam 2023/Assets/Scripts/Platform.cs
+++ Ico Jam 2023/Assets/Scripts/Platform.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public enum PlatformType
 {
@@ -14,13 +16,19 @@ public class Platform : MonoBehaviour
     [SerializeField] private PlatformType platformType;
     [SerializeField] private Sprite activeSprite;
     [SerializeField] private Sprite deActiveSprite;
+    [SerializeField] private List<Sprite> activeSpritesList;
+    [SerializeField] private List<Sprite> deActiveSpritesList;
     [SerializeField] private Vector2 target;
     [SerializeField] private float moveDuration;
     [SerializeField] private Ease moveEase;
+    [SerializeField] private float speed = 9;
     
     private Collider2D _collision;
     private SpriteRenderer _spriteRenderer;
     private bool _isOnX = true;
+    private bool _isOn = true;
+    private float _timer;
+    private int _index;
 
     private void Start()
     {
@@ -36,13 +44,30 @@ public class Platform : MonoBehaviour
         Timer.instance.onTimerEnd -= OnTimerEnd;
     }
 
+    private void Update()
+    {
+        _timer -= Time.deltaTime;
+        if (_timer <= 0)
+        {
+            var spritesAnimationList = _isOn ? activeSpritesList : deActiveSpritesList;
+            _spriteRenderer.sprite = spritesAnimationList[_index];
+            _index++;
+            if (_index >= spritesAnimationList.Count)
+                _index = 0;
+            _timer = 1f / Random.Range(speed - 2f, speed);
+        }
+    }
+    
     private void OnTimerEnd()
     {
         switch (platformType)
         {
             case PlatformType.Intangibility:
                 _collision.enabled = !_collision.enabled;
-                _spriteRenderer.sprite = _spriteRenderer.sprite == activeSprite ? deActiveSprite : activeSprite;
+                if(platformType != PlatformType.Intangibility)
+                    _spriteRenderer.sprite = _spriteRenderer.sprite == activeSprite ? deActiveSprite : activeSprite;
+                else
+                    _isOn = !_isOn;
                 break;
             case PlatformType.Moving:
                 if (_isOnX)
--- Ico Jam 2023/Assets/Scripts/TileAnimation.cs
+++ Ico Jam 2023/Assets/Scripts/TileAnimation.cs
@@ -6,14 +6,17 @@ public class TileAnimation : MonoBehaviour
 {
     [SerializeField] private List<Sprite> spritesAnimationList;
     [SerializeField] private float speed;
+    [SerializeField] private bool usingSpriteRenderer;
 
     private Image _image;
+    private SpriteRenderer _spriteRenderer;
     private float _timer;
     private int _index;
 
     private void Awake()
     {
         _image = GetComponent<Image>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
@@ -21,7 +24,10 @@ public class TileAnimation : MonoBehaviour
         _timer -= Time.deltaTime;
         if (_timer <= 0)
         {
-            _image.sprite = spritesAnimationList[_index];
+            if(usingSpriteRenderer)
+                _spriteRenderer.sprite = spritesAnimationList[_index];
+            else
+                _image.sprite = spritesAnimationList[_index];
             _index++;
             if (_index >= spritesAnimationList.Count)
                 _index = 0;
